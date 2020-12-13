3f025944e68b3a89e76b5f257452751637c0269c
LT <support@teran.tech>
2020-12-12 17:55:45 -0700

    Challenge 2 Completed.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public GameObject dogPrefab;
+   public GameObject dogPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+   private float lastKeyPress;
+
+   void Start()
+   {
+      //
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      // On spacebar press, send dog
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+         if (lastKeyPress + 1 <= Time.time)
+         {
+            lastKeyPress = Time.time;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
-        }
-    }
+         }
+      }
+   }
 }
