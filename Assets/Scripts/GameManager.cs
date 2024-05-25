8e36141940f6704933cddf1af6822c55d96cc5bb
LT <support@teran.tech>
2024-05-25 12:23:57 -0700

    Working on UI.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,26 +5,38 @@ using UnityEngine;
 
 namespace Prototype2 {
    public class GameManager : MonoBehaviour {
-      [SerializeField] private GameObject _titleScreen;
+      [SerializeField] private GameObject _titleScreen = default;
+      [SerializeField] private GameObject _gameScreen = default;
       [Header("Broadcasts on")]
-      [SerializeField] private VoidEventChannelSO _onStartGame;
+      [SerializeField] private VoidEventChannelSO _onStartGame = default;
       [Header("Listens on")]
-      [SerializeField] private VoidEventChannelSO _onGameOver;
+      [SerializeField] private VoidEventChannelSO _onGameOver = default;
+      [SerializeField] private VoidEventChannelSO _scoreIncrease = default;
+
+      private int score = 0;
 
       private void OnEnable() {
          _onGameOver?.Subscribe(EndGame);
+         _scoreIncrease?.Subscribe(ScoreIncrease);
       }
 
       private void OnDisable() {
          _onGameOver?.Unsubscribe(EndGame);
+         _scoreIncrease?.Unsubscribe(ScoreIncrease);
+      }
+
+      private void ScoreIncrease() {
+         score++;
       }
 
       public void StartGame() {
          _titleScreen?.SetActive(false);
+         _gameScreen?.SetActive(true);
          _onStartGame?.RaiseEvent();
       }
 
       public void EndGame() {
+         _gameScreen?.SetActive(false);
          _titleScreen?.SetActive(true);
       }
    }
